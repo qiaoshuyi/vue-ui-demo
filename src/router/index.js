18ceb7b9ba06cd032ffe2d72db792bab89ed8ca2
qiaoshuyi <qiaoshuyi@jd.com>
2018-07-12 15:51:37 +0800

    更新

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-Vue.use(Router)
+Vue.use(Router);
+
+const HelloWorld = () => import('@/components/HelloWorld');
 
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+      component: HelloWorld,
+    },
+  ],
+});
